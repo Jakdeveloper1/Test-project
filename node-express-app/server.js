20c23fb82586feea39b78637debd360e6027d006
Jakdeveloper1 <88589230+Jakdeveloper1@users.noreply.github.com>
2025-05-02 09:12:40 +0500

    Update server.js

--- node-express-app/server.js
+++ node-express-app/server.js
@@ -8,6 +8,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Main PAge\n');
+});
 app.get('/hello', (req, res) => {
   res.send('Hello world\n');
 });
